1d088aa9c55406d5dca865e0af7e10072046aac1
adriel alexander <adriel.alexs123@gmail.com>
2024-08-15 16:32:56 -0300

    desafio poo

--- Program.cs
+++ Program.cs
@@ -1,3 +1,17 @@
 ﻿using DesafioPOO.Models;
 
-// TODO: Realizar os testes com as classes Nokia e Iphone
+Console.WriteLine("Iphone");
+Smartphone iphone = new Iphone("(87)95412-6352", "XYZ-200", "001970-01-010000-0", 64);
+iphone.Ligar();
+iphone.ReceberLigacao();
+iphone.InstalarAplicativo("Youtube");
+
+Console.WriteLine();
+
+Console.WriteLine("Nokia");
+Smartphone nokia = new Nokia("(87)95412-6389", "AZ-400", "001970-01-010000-0", 64);
+nokia.Ligar();
+nokia.ReceberLigacao();
+nokia.InstalarAplicativo("Youtube Music");
+
+
